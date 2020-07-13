ab93969d2baa3da17f78c602d9d0c1ab12a27a97
ollieacademytbd <oliver.hagger@academytrainee.com>
2020-07-13 15:57:21 +0100

    Added product

--- src/main/java/com/qa/ims/persistence/domain/Product.java
+++ src/main/java/com/qa/ims/persistence/domain/Product.java
@@ -1,5 +1,78 @@
 package com.qa.ims.persistence.domain;
 
 public class Product {
-iikg
+	private int product_id;
+	private String product_name;
+	private double price;
+
+	public Product(int product_id, String product_name, double price) {
+		super();
+		this.product_id = product_id;
+		this.product_name = product_name;
+		this.price = price;
+	}
+
+	public int getProduct_id() {
+		return product_id;
+	}
+
+	public void setProduct_id(int product_id) {
+		this.product_id = product_id;
+	}
+
+	public String getProduct_name() {
+		return product_name;
+	}
+
+	public void setProduct_name(String product_name) {
+		this.product_name = product_name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public String toString() {
+		return "Product ID:" + product_id + " Product Name:" + product_name + " Price:" + price;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((product_name == null) ? 0 : product_name.hashCode());
+		result = prime * result + ((product_id == null) ? 0 : profuct_id.hashCode());
+		result = prime * result + ((price == null) ? 0 : price.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		if (product_id == null) {
+			if (other.product_id != null)
+				return false;
+		} else if (!product_id.equals(other.product_id))
+			return false;
+		if (product_name == null) {
+			if (other.product_name != null)
+				return false;
+		} else if (!product_name.equals(other.product_name))
+			return false;
+		if (price == null) {
+			if (other.price != null)
+				return false;
+		} else if (!price.equals(other.price))
+			return false;
+		return true;
 }
