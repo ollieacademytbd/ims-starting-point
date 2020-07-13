483ce46e2ea20c5eaaffc93179ae81f4c8070b0b
ollieacademytbd <oliver.hagger@academytrainee.com>
2020-07-13 16:23:18 +0100

    Added order page

--- src/main/java/com/qa/ims/persistence/domain/Order.java
+++ src/main/java/com/qa/ims/persistence/domain/Order.java
@@ -2,4 +2,98 @@ package com.qa.ims.persistence.domain;
 
 public class Order {
 
+	private Integer order_id;
+	private Integer customer_id;
+	private Integer product_id;
+	private Integer total;
+
+	public Order(Integer order_id, Integer customer_id, Integer product_id, Integer total) {
+		super();
+		this.order_id = order_id;
+		this.customer_id = customer_id;
+		this.product_id = product_id;
+		this.total = total;
+	}
+
+	public Integer getOrder_id() {
+		return order_id;
+	}
+
+	public void setOrder_id(Integer order_id) {
+		this.order_id = order_id;
+	}
+
+	public Integer getCustomer_id() {
+		return customer_id;
+	}
+
+	public void setCustomer_id(Integer customer_id) {
+		this.customer_id = customer_id;
+	}
+
+	public Integer getProduct_id() {
+		return product_id;
+	}
+
+	public void setProduct_id(Integer product_id) {
+		this.product_id = product_id;
+	}
+
+	public Integer getTotal() {
+		return total;
+	}
+
+	public void setTotal(Integer total) {
+		this.total = total;
+	}
+
+	public String toString() {
+		return "OrderID:" + order_id + " CustomerID:" + customer_id + " ProductID:" + product_id + " Total:" + total;
+
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((order_id == null) ? 0 : order_id.hashCode());
+		result = prime * result + ((product_id == null) ? 0 : product_id.hashCode());
+		result = prime * result + ((customer_id == null) ? 0 : customer_id.hashCode());
+		result = prime * result + ((total == null) ? 0 : total.hashCode());
+		return result;
+
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		if (order_id == null) {
+			if (other.order_id != null)
+				return false;
+		} else if (!order_id.equals(other.order_id))
+			return false;
+		if (product_id == null) {
+			if (other.product_id != null)
+				return false;
+		} else if (!product_id.equals(other.product_id))
+			return false;
+		if (customer_id == null) {
+			if (other.customer_id != null)
+				return false;
+		} else if (!customer_id.equals(other.customer_id))
+			return false;
+		if (total == null) {
+			if (other.total != null)
+				return false;
+		} else if (!total.equals(other.total))
+			return false;
+		return true;
+	}
+
 }
